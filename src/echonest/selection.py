7b186cd46533448be51eeaef5d7f9f89b96e3a56
Adam Lindsay <a.lindsay@gmail.com>
2008-09-15 23:35:11 +0000

    Added selection.py with simple content-based selection functions. Added examples/selection/, which demonstrates AudioQuantumList.that()

---
+++ src/echonest/selection.py
@@ -0,0 +1,72 @@
+# selection.py
+# selection filters as utilities for audio.py
+# for convenience, intended to be imported as:
+#   from selection import *
+# if it's not clear, they are all named to be called as analysis.segments.that...
+
+def are_contained_by_range(start, end):
+    def fun(x): 
+        if x.start > start and x.start + x.duration < end: 
+            return x
+    return fun
+
+def are_contained_by(aq):
+    'expects an AudioQuantum, with start and duration'
+    def fun(x):
+        if x.start > aq.start and x.start + x.duration < aq.start + aq.duration: 
+            return x
+    return fun
+
+def overlap_range(start, end):
+    def fun(x): 
+        if x.start + x.duration > start and x.start < end:
+            return x
+    return fun
+
+def overlap(aq):
+    def fun(x): 
+        if x.start + x.duration > aq.start and x.start < aq.start + aq.duration:
+            return x
+    return fun
+
+
+def end_during_range(start, end):
+    def fun(x):
+        if x.start + x.duration > start and x.start + x.duration < end:
+            return x
+    return fun
+
+def end_during(aq):
+    def fun(x):
+        if x.start + x.duration > aq.start and x.start + x.duration < aq.start + aq.duration:
+            return x
+    return fun
+
+def start_during_range(start, end):
+    def fun(x):
+        if x.start > start and x.start < end:
+            return x
+    return fun
+
+def start_during(aq):
+    def fun(x):
+        if x.start > aq.start and x.start < aq.start + aq.duration:
+            return x
+    return fun
+
+def contain_point(point):
+    def fun(x): 
+        if point > x.start and point < x.start + x.duration:
+            return x
+    return fun
+
+def have_pitch_max(pitchmax):
+    def fun(x):
+        pitches = x.pitches
+        if reduce(all, [pitches[pitchmax] >= p for p in pitches]):
+            return x
+    return fun
+
+# 
+def all(x, y):
+    return x and y
