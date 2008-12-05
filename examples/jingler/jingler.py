ada636c7ece1384923a9d9e6232d2865153d2cb4
Tristan Jehan <tristan@echonest.com>
2008-12-05 17:07:06 +0000

    jingler now with audio compressor/limiter

--- examples/jingler/jingler.py
+++ examples/jingler/jingler.py
@@ -22,26 +22,27 @@ Reference:
 """
 
 # constants
-SLEIGH_OFFSET = -0.2 # seconds
+SLEIGH_OFFSET = [-0.2, -0.075] # seconds
 SIGNAL_OFFSET = -1.0 # seconds
 SANTA_OFFSET = -6.25 # seconds
-MIN_BEAT_DURATION = 0.3 # seconds
+SLEIGH_THRESHOLD = 0.420 # seconds
+MIN_BEAT_DURATION = 0.280 # seconds
+MAX_BEAT_DURATION = 0.560 # seconds
+LIMITER_THRESHOLD = 29491 # 90% of the dynamic range
+LIMITER_COEFF = 0.2 # compresion curve
 
 # samples
 soundsPath = "sounds/"
 
-sleighBellSounds = map(lambda x: audio.AudioData(os.path.join(soundsPath, "sleighBell%s.wav" % x), sampleRate=44100, numChannels=2), range(1))
+snowSantaSounds  = map(lambda x: audio.AudioData(os.path.join(soundsPath, "snowSanta%s.wav" % x), sampleRate=44100, numChannels=2), range(1))
+sleighBellSounds = map(lambda x: audio.AudioData(os.path.join(soundsPath, "sleighBell%s.wav" % x), sampleRate=44100, numChannels=2), range(2))
 signalBellSounds = map(lambda x: audio.AudioData(os.path.join(soundsPath, "signalBell%s.wav" % x), sampleRate=44100, numChannels=2), range(3))
-snowSantaSounds = map(lambda x: audio.AudioData(os.path.join(soundsPath, "snowSanta%s.wav" % x), sampleRate=44100, numChannels=2), range(1))
+
 
 # some math useful for normalization
 def linear(input, in1, in2, out1, out2):
     return ((input-in1) / (in2-in1)) * (out2-out1) + out1
 
-def exp(input, in1, in2, out1, out2, coeff):
-    if (input <= in1): return out1
-    if (input >= in2): return out2
-    return pow( ((input-in1) / (in2-in1)) , coeff ) * (out2-out1) + out1
 
 class Jingler:
     def __init__(self, audio, loudness, endOfFadeIn, beats, sections):
@@ -52,7 +53,7 @@ class Jingler:
         self.audiodata = numpy.array(mf.readall(), dtype=numpy.int32)
         self.audiodata *= linear(loudness, -2, -12, 0.5, 1.5) * 0.75
         self.audiodata = self.audiodata.reshape(len(self.audiodata) / 2, 2)
-        print "Loaded/Normalized audio in %.3f secs" % (time.time() - t1)
+        print "Loaded/Normalized audio input in %.3f secs" % (time.time() - t1)
         self.audiorate = mf.samplerate()    
         self.audiochannels = 2 # force it
         self.beats = beats
@@ -60,41 +61,51 @@ class Jingler:
         self.duration = len(self.audiodata) / self.audiorate
         self.endOfFadeIn = endOfFadeIn
 
-
     def run(self, out):
         # sequence and mix
         t1 = time.time()
+        print "Sequencing and mixing..."
         self.sequence(sleighBellSounds)
-        self.normalize()
+        print "Normalizing audio output..."
+        #self.limiter() # slow but can be used in place of self.normalize()
+        self.normalize() # remove this line if you use self.limiter() instead
         # at this point self.audiodata is down to a int16 array
-        print "Sequenced and mixed in %.3f secs" % (time.time() - t1)
+        print "Sequenced, mixed and normalized in %.3f secs" % (time.time() - t1)
         # save
         t1 = time.time()
         print "Encoding mp3..."
         self.encode(out)
         print "Encoded in %.3f secs" % (time.time() - t1)
 
-
+    # simple normalization that prevents clipping. There can be a little bit of a volume drop.
+    # to prevent volume drops, use self.limiter() instead, however it is slower.
     def normalize(self):
         factor = 32767.0 / numpy.max(numpy.absolute(self.audiodata.flatten()))
         if factor < 1:
             # we return to 16 bit arrays
             self.audiodata = numpy.array(self.audiodata * factor, dtype=numpy.int16)
 
-
     def sequence(self, chops):
-        # divide the duration by two if it sounds too fast
+        # adjust sounds and durations
         if self.beats[4].duration < MIN_BEAT_DURATION: stride = 2
         else: stride = 1
+        if self.beats[4].duration > MAX_BEAT_DURATION: multiplier = 0.5
+        else: multiplier = 1
+        if self.beats[4].duration * stride * multiplier > SLEIGH_THRESHOLD: sleighBellIndex = 0
+        else: sleighBellIndex = 1
+
         # add sleigh bells on the beats
         for i in range(0,len(self.beats),stride):
             beat = self.beats[i]
             # let's put a little bit of jitter in the volume
             vol = 0.6 + random.randint(-100,100) / 1000.0
-            sample = sleighBellSounds[0]
+            sample = sleighBellSounds[sleighBellIndex]
             # let's stop jingling 5 seconds before the end anyhow
-            if beat.start+SLEIGH_OFFSET + 5 < self.duration:
-                self.mix(beat.start+SLEIGH_OFFSET, seg=sample, volume=vol)
+            if beat.start+SLEIGH_OFFSET[sleighBellIndex]+5.0 < self.duration:
+                self.mix(beat.start+SLEIGH_OFFSET[sleighBellIndex], seg=sample, volume=vol)
+            if multiplier == 0.5:
+                self.mix(beat.start+beat.duration/2.0+SLEIGH_OFFSET[sleighBellIndex], seg=sample, volume=vol/2.0)
+                
         # add signal bells on section changes
         for section in self.sections[1:]:
             sample = signalBellSounds[random.randint(0,1)]
@@ -107,7 +118,6 @@ class Jingler:
         sample = snowSantaSounds[0]
         self.mix(start=self.duration+SANTA_OFFSET, seg=sample, volume=1.0)
 
-
     def mix(self, start=None, seg=None, volume=0.3, pan=0.):
         # this assumes that the audios have the same samplerate/numchannels
         startsample = int(start * self.audiorate)
@@ -117,6 +127,27 @@ class Jingler:
             self.audiodata[startsample:startsample+len(seg.data)] += seg.data[0:] # mix
             #numpy.clip(self.audiodata[startsample:startsample+len(seg.data)], -32767, 32767)
 
+    def curve_int16(self, x):
+        return int(numpy.power((x-LIMITER_THRESHOLD)/3276, LIMITER_COEFF) * 3276 + LIMITER_THRESHOLD)
+        #return numpy.power((x-LIMITER_THRESHOLD)/(32767-LIMITER_THRESHOLD), LIMITER_COEFF) * (32767-LIMITER_THRESHOLD) + LIMITER_THRESHOLD)
+
+    def limit_int16(self, x):
+        if x > LIMITER_THRESHOLD:
+            tmp = self.curve_int16(x)
+            if tmp > 32767: return 32767
+            else: return tmp
+        elif x < -LIMITER_THRESHOLD:
+            value = -x
+            tmp = self.curve_int16(value)
+            if tmp > 32767: return -32767
+            else: return -tmp
+        return x
+
+    # simple attempt at compressing and limiting the mixed signal to avoid clipping
+    # this is a bit slower than I would like (roughly 10 secs) but it works fine
+    def limiter(self):
+        vec_limiter = numpy.vectorize(self.limit_int16, otypes=[numpy.int16])
+        self.audiodata = vec_limiter(self.audiodata)
 
     def encode(self, mp3_path):
         sampwidth = 2
@@ -163,9 +194,9 @@ def main( inputFilename, outputFilename ) :
 
     # Upload track for analysis.
     print 'uploading audio file...'
-    track = audio.ExistingTrack(inputFilename).analysis
+    track = audio.ExistingTrack(inputFilename).analysis # This is useful once you've uploaded a track once and it's been registered
     #track = audio.AudioFile(inputFilename).analysis
-    
+
     # Get loudness.
     print 'getting loudness...'
     loudness = track.loudness
@@ -176,7 +207,7 @@ def main( inputFilename, outputFilename ) :
     beats = track.beats
     # Check that there are beats in the song
     if len(beats) < 5: 
-        print 'not enough beats in this songs...'
+        print 'not enough beats in this song...'
         sys.exit(-2)
     # Get sections.
     print 'getting sections...'
@@ -192,6 +223,7 @@ def main( inputFilename, outputFilename ) :
     # Finalize output file.
     print 'finalizing output file...'
     write_tags(outputFilename, metadata)
+    print 'Done.'
 
 
 if __name__ == '__main__':
