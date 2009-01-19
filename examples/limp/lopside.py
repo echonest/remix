7b5d1a15ea120304e85680c31967bac7404c4c01
Adam Lindsay <a.lindsay@gmail.com>
2009-01-19 15:10:28 +0000

    added example limp/lopside.py to demonstrate rhythm navigation

---
+++ examples/limp/lopside.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf=8
+
+"""
+lopside.py
+
+Cut out the final beat or tatum in each bar.
+Demonstrates the beat hierarchy navigation in AudioQuantum
+
+Originally by Adam Lindsay, 2009-01-19.
+"""
+import echonest.audio as audio
+
+usage = """
+Usage: 
+    python lopside.py <tatum|beat> <inputFilename> <outputFilename>
+
+Example:
+    python tonic.py beat aha.mp3 ahawaltz.wav
+"""
+
+
+def main(units, inputFile, outputFile):
+    audiofile = audio.LocalAudioFile(inputFile)
+    collect = audio.AudioQuantumList()
+    for b in audiofile.analysis.bars:                
+        # all but the last beat
+        collect.extend(b.children()[0:-1])
+        if units.startswith("tatum"):
+            # all but the last portion of the last beat
+            collect.extend(b.children()[-1].children()[0:-1])
+    
+    out = audio.getpieces(audiofile, collect)
+    out.save(outputFile)
+
+if __name__ == '__main__':
+    import sys
+    try:
+        units = sys.argv[-3]
+        inputFilename = sys.argv[-2]
+        outputFilename = sys.argv[-1]
+    except:
+        print usage
+        sys.exit(-1)
+    main(units, inputFilename, outputFilename)
