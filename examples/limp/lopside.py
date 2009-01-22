1484c2c73ddf474ef8eb1d2d00d2978d20ec9b1b
Adam Lindsay <a.lindsay@gmail.com>
2009-01-22 15:51:21 +0000

    Changed examples/limp/lopside.py to cut out last half beat instead of last tatum.

--- examples/limp/lopside.py
+++ examples/limp/lopside.py
@@ -4,7 +4,7 @@
 """
 lopside.py
 
-Cut out the final beat or tatum in each bar.
+Cut out the final beat or group of tatums in each bar.
 Demonstrates the beat hierarchy navigation in AudioQuantum
 
 Originally by Adam Lindsay, 2009-01-19.
@@ -27,9 +27,9 @@ def main(units, inputFile, outputFile):
         # all but the last beat
         collect.extend(b.children()[0:-1])
         if units.startswith("tatum"):
-            # all but the last portion of the last beat
-            collect.extend(b.children()[-1].children()[0:-1])
-    
+            # all but the last half (round down) of the last beat
+            half = - (len(b.children()[-1].children()) // 2)
+            collect.extend(b.children()[-1].children()[0:half])    
     out = audio.getpieces(audiofile, collect)
     out.save(outputFile)
 
