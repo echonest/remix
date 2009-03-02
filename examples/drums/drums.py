06d5723ccd9038cd8a1e6ed295ec72b44818cd53
Ben Lacker <benlacker@gmail.com>
2009-03-02 23:49:03 +0000

    more robust handling of different drum loops

--- examples/drums/drums.py
+++ examples/drums/drums.py
@@ -17,10 +17,10 @@ import echonest.audio as audio
 
 usage="""
 Usage:
-    python drums.py <inputfilename> <outputfilename> [<drumIntensity>]
+    python drums.py <inputfilename> <breakfilename> <outputfilename> [<drumIntensity>]
 
 Example:
-    python drums.py HereComesTheSun.mp3 HereComeTheDrums.mp3 0.6
+    python drums.py HereComesTheSun.mp3 breaks/assemblyline.wav HereComeTheDrums.mp3 0.6
     
 Drum Intensity defaults to 0.5.
 """
@@ -34,26 +34,46 @@ def mono_to_stereo(audio_data):
     audio_data.numChannels = 2
     return audio_data
 
-def add_drums(input_filename,output_filename,mix=0.5):
+def split_break(breakfile,n):
+    drum_data = []
+    start = 0
+    for i in range(n):
+        start = int((len(breakfile) * (i))/n)
+        end = int((len(breakfile) * (i+1))/n)
+        ndarray = breakfile.data[start:end]
+        new_data = audio.AudioData(ndarray=ndarray,
+                                    sampleRate=breakfile.sampleRate,
+                                    numChannels=breakfile.numChannels)
+        drum_data.append(new_data)
+    return drum_data
+
+def add_drums(input_filename,output_filename,break_filename,break_parts,measures):
     audiofile = audio.LocalAudioFile(input_filename)
     sample_rate = audiofile.sampleRate
-    breakfile = audio.LocalAudioFile(BREAK_FILENAME)
+    breakfile = audio.LocalAudioFile(break_filename)
     if breakfile.numChannels == 1:
         breakfile = mono_to_stereo(breakfile)
     num_channels = audiofile.numChannels
-    drums = breakfile.analysis.tatums[:16]
+    drum_data = split_break(breakfile,break_parts)
+    hits_per_beat = int(break_parts/(4 * measures))
     bars = audiofile.analysis.bars
     out_shape = (len(audiofile)+100000,num_channels)
     out = audio.AudioData(shape=out_shape, sampleRate=sample_rate,
                             numChannels=num_channels)
-    for bar in bars:
+    for bar in bars[:-1]:
         beats = bar.children()
         for i in range(len(beats)):
-            tats = range((i % 4) * 4, ((i % 4) * 4) + 4)
-            drum_samps = sum([len(breakfile[drums[x]]) for x in tats])
+            try:
+                break_index = ((bar.local_context()[0] %\
+                                measures) * 4) + (i % 4)
+            except ValueError:
+                break_index = i % 4
+            tats = range((break_index) * hits_per_beat,
+                        (break_index + 1) * hits_per_beat)
+            drum_samps = sum([len(drum_data[x]) for x in tats])
             beat_samps = len(audiofile[beats[i]])
             beat_shape = (beat_samps,num_channels)
-            tat_shape = (float(beat_samps/4),num_channels)
+            tat_shape = (float(beat_samps/hits_per_beat),num_channels)
             beat_data= audio.AudioData(shape=beat_shape,
                                         sampleRate=sample_rate,
                                         numChannels=num_channels)
@@ -61,34 +81,40 @@ def add_drums(input_filename,output_filename,mix=0.5):
                 tat_data= audio.AudioData(shape=tat_shape,
                                             sampleRate=sample_rate,
                                             numChannels=num_channels)
-                if drum_samps > beat_samps/4:
-                    # truncate drum hits to fit beat length
-                    tat_data.data = breakfile[drums[j]].data[:len(tat_data)]
-                elif drum_samps < beat_samps/4:
-                    # space out 16th notes of drums to fit beat length
-                    tat_data.append(breakfile[drums[j]])
+                if drum_samps > beat_samps/hits_per_beat:
+                # truncate drum hits to fit beat length
+                    tat_data.data = drum_data[j].data[:len(tat_data)]
+                elif drum_samps < beat_samps/hits_per_beat:
+                # space out drum hits to fit beat length
+                    tat_data.append(drum_data[j])
                 tat_data.endindex = len(tat_data)
                 beat_data.append(tat_data)
                 del(tat_data)
-            # account for round errors
+            # account for rounding errors
             beat_data.endindex = len(beat_data)
-            mixed_beat = audio.mix(beat_data,audiofile[beats[i]],mix=mix)
+            mixed_beat = audio.mix(beat_data,audiofile[beats[i]],mix=0.5)
             del(beat_data)
             out.append(mixed_beat)
+    finale = bars[-1].start + bars[-1].duration
+    last = audio.AudioQuantum(audiofile.analysis.bars[-1].start,
+                            audiofile.analysis.duration - 
+                              audiofile.analysis.bars[-1].start)
+    last_data = audio.getpieces(audiofile,[last])
+    out.append(last_data)
     out.encode(output_filename)
 
 def main():
     try:
         input_filename = sys.argv[1]
-        output_filename = sys.argv[2]
-        if len(sys.argv) == 4:
-            mix = sys.argv[3]
-        else:
-            mix = 0.5
+        break_filename = sys.argv[2]
+        output_filename = sys.argv[3]
+        break_parts = int(sys.argv[4])
+        measures = int(sys.argv[5])
     except:
         print usage
         sys.exit(-1)
-    add_drums(input_filename,output_filename,mix=mix)
+    add_drums(input_filename,output_filename,break_filename,
+                break_parts=break_parts,measures=measures)
 
 if __name__=='__main__':
     tic = time.time()
