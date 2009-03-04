0f43c953b97583fc4adc1da8a3853b595a6b0be1
Ben Lacker <benlacker@gmail.com>
2009-03-04 16:15:06 +0000

    Defaults to adding a second-long space between audio quanta.

--- examples/quanta/quanta.py
+++ examples/quanta/quanta.py
@@ -14,15 +14,18 @@ import echonest.audio as audio
 
 usage = """
 Usage: 
-    python quanta.py <segments|tatums|beats|bars|sections> <inputfilename> <outputfilename>
+    python quanta.py <segments|tatums|beats|bars|sections> <inputfilename> <outputfilename> [e]
 
 Example:
     python quanta.py beats SingleLadies.mp3 SingleLadiesBeats.mp3
+    
+The 'e' flag, inserts a silence equal in duration to the preceding audio quantum.
+Otherwise, each silence is one second long.
 """
 
 
 
-def main(input_filename,output_filename,quanta) :
+def main(input_filename,output_filename,quanta,equal_silence):
     audio_file = audio.LocalAudioFile(input_filename)
     if quanta == 'segments':
         chunks = audio_file.analysis.segments
@@ -36,12 +39,21 @@ def main(input_filename,output_filename,quanta) :
         chunks = audio_file.analysis.sections
     num_channels = audio_file.numChannels
     sample_rate = audio_file.sampleRate
-    new_shape = ((audio_file.data.shape[0]*2)+100000,audio_file.data.shape[1])
+    if equal_silence:
+        new_shape = ((audio_file.data.shape[0]*2)+100000,
+                        audio_file.data.shape[1])
+    else:
+        new_shape = (audio_file.data.shape[0]+(len(chunks)*44100)+10000,
+                        audio_file.data.shape[1])
     out = audio.AudioData(shape=new_shape,sampleRate=sample_rate,
                             numChannels=num_channels)
     for chunk in chunks:
         chunk_data = audio_file[chunk]
-        silence = audio.AudioData(shape=chunk_data.data.shape,
+        if equal_silence:
+            silence_shape = chunk_data.data.shape
+        else:
+            silence_shape = (44100,chunk_data.data.shape[1])
+        silence = audio.AudioData(shape=silence_shape,
                                 sampleRate=sample_rate,
                                 numChannels=num_channels)
         out.append(chunk_data)
@@ -54,7 +66,11 @@ if __name__ == '__main__':
         quanta = sys.argv[1]
         input_filename = sys.argv[2]
         output_filename = sys.argv[3]
+        if len(sys.argv) == 5:
+            equal_silence = True
+        else:
+            equal_silence = False
     except :
         print usage
         sys.exit(-1)
-    main(input_filename,output_filename,quanta)
+    main(input_filename,output_filename,quanta,equal_silence)
