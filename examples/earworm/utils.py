db8545578ab08eac957f42ee9fffbb9b291f8a22
Jason Sundram <jsundram@gmail.com>
2010-04-22 21:48:08 +0000

    Earworm exercises action.py, and makes a song longer, or shorter, or even infinite.

---
+++ examples/earworm/utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+utils.py
+
+Created by Jason Sundram, on 2010-04-05.
+"""
+
+def flatten(l):
+    """ Converts a list of tuples to a flat list.
+        e.g. flatten([(1,2), (3,4)]) => [1,2,3,4]
+    """
+    return [item for pair in l for item in pair]
+
+def tuples(l, n=2):
+    """ returns n-tuples from l.
+        e.g. tuples(range(4), n=2) -> [(0, 1), (1, 2), (2, 3)]
+    """
+    return zip(*[l[i:] for i in range(n)])
+
+def rows(m):
+    """returns the # of rows in a numpy matrix"""
+    return m.shape[0]
