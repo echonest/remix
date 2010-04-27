e461d7fb64be951127a8097d1a5c6e91562d4909
Jason Sundram <jsundram@gmail.com>
2010-04-27 15:08:38 +0000

    Helpful error message if your track isn't found.

--- examples/earworm/earworm.py
+++ examples/earworm/earworm.py
@@ -477,6 +477,9 @@ def main():
     track = None
     if len(args) == 2:
         track = find_track(args[0], args[1])
+        if not track:
+            print "Couldn't find %s by %s" % (args[0], args[1])
+            return 1
     else:
         mp3 = args[0]
         
