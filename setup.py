dd4c4266e72db31958bbfea3aa15c9629b063db3
Jason Sundram <jsundram@gmail.com>
2010-05-10 20:59:07 +0000

    cleanup, refactor, inc version number, include dirac.

--- setup.py
+++ setup.py
@@ -4,103 +4,92 @@ __version__ = "$Revision: 0 $"
 # $Source$
 
 from distutils.core import setup, Extension
-import os, glob,sys
-try:
-    import numpy
-except:
-    sys.exit("numpy is required to use remix")
+import os, glob
+import numpy
 
-# pysoundtouch
-sources = ['AAFilter.cpp',
-           'FIFOSampleBuffer.cpp',
-           'FIRFilter.cpp',
-           'RateTransposer.cpp',
-           'SoundTouch.cpp',
-           'TDStretch.cpp',
-           'BPMDetect.cpp',
-           'PeakFinder.cpp',
-           'mmx_optimized.cpp',
-           'sse_optimized.cpp'
-           ]
-
-sources_gcc = ['cpu_detect_x86_gcc.cpp',
-               ]
-
-sources_win = ['cpu_detect_x86_win.cpp',
-               '3dnow_win.cpp',
-               ]
-
-extra_compile_args = []
-
-is_linux = False
-is_mac =False
-is_windows = False
-major_version_string = ".".join([str(x) for x in sys.version_info[0:2]]) # 2.6, 2.5 etc
-
-# Is this is posix platform, or is it windows?
-if hasattr(os, 'uname'):
-    if(os.uname()[0] == "Darwin"):
-        is_mac =True
-    else:
-        is_linux = True
-    sources += sources_gcc
-    extra_compile_args=['-fcheck-new', '-O3']
-else:
-    is_windows = True
-    sources += sources_win
+def get_os():
+    """returns is_linux, is_mac, is_windows"""
+    if hasattr(os, 'uname'):
+        if os.uname()[0] == "Darwin":
+            return False, True, False
+        return True, False, False
+    return False, False, True
     
-libSoundTouch_sources = [os.path.join('external/pysoundtouch14/libsoundtouch', i) for i in sources]
+is_linux, is_mac, is_windows = get_os()
 
-soundtouch = Extension('soundtouch', sources=['external/pysoundtouch14/soundtouchmodule.cpp',] + libSoundTouch_sources, extra_compile_args = extra_compile_args, include_dirs = [numpy.get_include(), numpy.get_numarray_include()])
+def get_dirac():
+    link_args = ['-framework Carbon'] if is_mac else []
+    
+    pydirac = os.path.join('external', 'pydirac225')
+    lib_sources = [os.path.join(pydirac,'diracmodule.cpp'), os.path.join(pydirac, 'source', 'Dirac_LE.cpp')]
+    
+    platform = os.uname()[0] if hasattr(os, 'uname') else 'Windows'
+    return Extension(   'dirac',
+                        sources = lib_sources,
+                        extra_compile_args = [],
+                        include_dirs = ['source', numpy.get_include(), numpy.get_numarray_include()],
+                        libraries = ['Dirac'],
+                        library_dirs = [os.path.join(pydirac, 'libs', platform)],
+                        extra_link_args = link_args,
+                     )
 
-
-
+def get_soundtouch():
+    sources = ['AAFilter.cpp',
+               'FIFOSampleBuffer.cpp',
+               'FIRFilter.cpp',
+               'RateTransposer.cpp',
+               'SoundTouch.cpp',
+               'TDStretch.cpp',
+               'BPMDetect.cpp',
+               'PeakFinder.cpp',
+               'mmx_optimized.cpp',
+               'sse_optimized.cpp'
+               ]
+    
+    extra_compile_args = []
+    
+    if is_linux or is_mac:
+        sources += ['cpu_detect_x86_gcc.cpp']
+        extra_compile_args=['-fcheck-new', '-O3']
+    else:
+        sources += ['cpu_detect_x86_win.cpp', '3dnow_win.cpp']
+    pysoundtouch = os.path.join('external','pysoundtouch14','libsoundtouch')
+    lib_sources = [os.path.join(pysoundtouch, i) for i in sources]
+    lib_sources += [os.path.join('external','pysoundtouch14','soundtouchmodule.cpp')]
+    return Extension(   'soundtouch', 
+                        sources = lib_sources,
+                        extra_compile_args = extra_compile_args, 
+                        include_dirs = [numpy.get_include(), numpy.get_numarray_include()]
+                    )
 
 all_data_files = []
-if(is_mac):
+if is_mac:
     all_data_files  = [('/usr/local/bin',['external/en-ffmpeg/mac/en-ffmpeg','external/youtube-dl/youtube-dl'])]
-if(is_linux):
+if is_linux:
     all_data_files  = [('/usr/local/bin',['external/youtube-dl/youtube-dl'])]
-if(is_windows):
+if is_windows:
     all_data_files  = [('.',['external\\en-ffmpeg\\win\\en-ffmpeg.exe','external\\youtube-dl\\youtube-dl'])]
 
 
-if(is_mac or is_linux):
-    dest_prefix = 'echo-nest-remix-'
-    for example_dir in glob.glob('examples/*'):
-        example_files = glob.glob(example_dir+'/*')
-        actual_files = []
-        sub_path_tuples = []
-        for example_file in example_files:
-            files_within_example_file = glob.glob(example_file+'/*')
-            if not any(files_within_example_file):
-                actual_files.append(example_file)
-            else:
-                sub_path_tuples.append((dest_prefix+example_file, files_within_example_file))
-        all_data_files.append((dest_prefix+example_dir, actual_files))
-        if any(sub_path_tuples):
-            all_data_files.extend(sub_path_tuples)
+dest_prefix = 'echo-nest-remix-'
+for example_dir in glob.glob(os.path.join('examples', '*')):
+    example_files = glob.glob(os.path.join(example_dir, '*'))
+    actual_files = []
+    sub_path_tuples = []
+    for example_file in example_files:
+        files_within_example_file = glob.glob(os.path.join(example_file, '*'))
+        if not any(files_within_example_file):
+            actual_files.append(example_file)
+        else:
+            sub_path_tuples.append((dest_prefix+example_file, files_within_example_file))
+    all_data_files.append((dest_prefix + example_dir, actual_files))
+    if any(sub_path_tuples):
+        all_data_files.extend(sub_path_tuples)
 
-if(is_windows):
-    dest_prefix = "echo-nest-remix-"
-    for example_dir in glob.glob('examples\\*'):
-        example_files = glob.glob(example_dir+'\\*')
-        actual_files = []
-        sub_path_tuples = []
-        for example_file in example_files:
-            files_within_example_file = glob.glob(example_file+'\\*')
-            if not any(files_within_example_file):
-                actual_files.append(example_file)
-            else:
-                sub_path_tuples.append((dest_prefix+example_file, files_within_example_file))
-        all_data_files.append((dest_prefix+example_dir, actual_files))
-        if any(sub_path_tuples):
-            all_data_files.extend(sub_path_tuples)
-    
 
 setup(name='The Echo Nest Remix API',
-      ext_modules = [soundtouch],
-      version='1.2',
+      ext_modules = [get_soundtouch(), get_dirac()],
+      version='1.3',
       description='Make things with music.',
       author='Ben Lacker',
       author_email='blacker@echonest.com',
