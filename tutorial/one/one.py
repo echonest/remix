937c46c9ee026439a0382f6dec8bfa5c2d466cf1
Thor Kell <djfractal@gmail.com>
2012-09-09 20:40:12 -0400

    Drafted tutorial text, added comments to one.py

--- tutorial/one/one.py
+++ tutorial/one/one.py
@@ -5,8 +5,9 @@ one.py
 
 Digest only the first beat of every bar.
 
-By Ben Lacker, 2009-02-18.
+By Ben Lacker, 2009-02-18.  
 """
+# The imports 'import echonest.audio as audio' is the key one. 
 import echonest.audio as audio
 from echonest.selection import have_pitch_max,have_pitches_max
 
@@ -19,20 +20,38 @@ Example:
 """
 
 def main(input_filename, output_filename):
+    # This takes your input track, sends it to the analyzer, and returns the results.  
     audiofile = audio.LocalAudioFile(input_filename)
+
+    # This gets a list of every bar in the track.  
+    # You can manipulate this just like any other Python list!
     bars = audiofile.analysis.bars
+
+    # This makes a new list of "AudioQuantums".  
+    # Those are just any discrete chunk of audio:  bars, beats, etc
     collect = audio.AudioQuantumList()
+
+    # This loop puts the first item in the children of each bar into the new list. 
+    # A bar's children are beats!  Simple as that. 
     for bar in bars:
         collect.append(bar.children()[0])
+
+    # This assembles the pieces of audio defined in collect out of the analyzed audio file.
     out = audio.getpieces(audiofile, collect)
+    
+    # This writes the newly created to the given file.  
     out.encode(output_filename)
 
+
+    # The wrapper for the script.  
 if __name__ == '__main__':
     import sys
     try:
+        # This gets the filenames to read from and write to.
         input_filename = sys.argv[1]
         output_filename = sys.argv[2]
     except:
+        # If things go wrong, exit!
         print usage
         sys.exit(-1)
     main(input_filename, output_filename)
