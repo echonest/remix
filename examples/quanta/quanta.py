6de036536f8d95f1bed3da313a66656537197bb6
tkell <thor.kell@mail.mcgill.ca>
2012-09-26 20:29:04 -0400

    Fixed quanta.py so that silences insert properly.

--- examples/quanta/quanta.py
+++ examples/quanta/quanta.py
@@ -7,7 +7,7 @@ quanta.py
 Insert silences between a song's segments, tatums, beats, or sections.
 Demonstrates the Remix API's handling of audio quanta.
 
-By Ben Lacker 2009-3-4.
+By Ben Lacker 2009-3-4, updated by Thor Kell, 2012-9-26
 """
 import sys
 
@@ -27,7 +27,7 @@ Otherwise, each silence is one second long.
 ACCEPTED_UNITS = ["segments", "tatums", "beats", "bars", "sections"]
 
 def main(input_filename, output_filename, units, equal_silence):
-    audio_file = audio.LocalAudioFile(input_filename)
+    audio_file = audio.LocalAudioFile(input_filename)   
     chunks = audio_file.analysis.__getattribute__(units)
     num_channels = audio_file.numChannels
     sample_rate = audio_file.sampleRate
@@ -37,17 +37,17 @@ def main(input_filename, output_filename, units, equal_silence):
     else:
         new_shape = (audio_file.data.shape[0]+(len(chunks) * 44100) + 10000,
                         audio_file.data.shape[1])
-    out = audio.AudioData(shape=new_shape, sampleRate=sample_rate,
-                            numChannels=num_channels)
+    out = audio.AudioData(shape=new_shape, sampleRate=sample_rate, numChannels=num_channels)
+
     for chunk in chunks:
         chunk_data = audio_file[chunk]
         if equal_silence:
             silence_shape = chunk_data.data.shape
         else:
-            silence_shape = (44100, chunk_data.data.shape[1])
-        silence = audio.AudioData(shape=silence_shape,
-                                sampleRate=sample_rate,
-                                numChannels=num_channels)
+            silence_shape = (44100, audio_file.data.shape[1])
+        silence = audio.AudioData(shape=silence_shape, sampleRate=sample_rate, numChannels=num_channels)
+        silence.endindex = silence.data.shape[0]
+
         out.append(chunk_data)
         out.append(silence)
     out.encode(output_filename)
