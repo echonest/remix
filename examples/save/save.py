47a1e685c49e0976ad82569af677fafa3d6d25b3
tkell <thor.kell@mail.mcgill.ca>
2012-10-24 14:58:56 -0400

    Updated save.py

--- examples/save/save.py
+++ examples/save/save.py
@@ -4,7 +4,7 @@
 save.py
 
 Save an Echo Nest analysis as a local file. You can save analysis by using .save()
-You can then load them by simply calling audio.LocalAudioFile('the_saved_file')
+You can then load them by simply calling audio.LocalAudioFile('the_file.analysis.en')
 
 Note that save() saves to the same location as the initial file, and 
 creates a corresponding .wav file.  Moving the .wav file will break things!
@@ -17,31 +17,23 @@ Usage:
     python save.py <input_filename> <output_filename>
 
 Example:
-    python save.py EverythingIsOnTheOne.mp3 EverythingIsReallyOnTheOne.mp3
+    python save.py SaveMe.mp3 
 """
 
 import echonest.audio as audio
 
-def main(input_filename, output_filename):
-    # This is where the magic happens.  
+def main(input_filename):
     audiofile = audio.LocalAudioFile(input_filename)
     audiofile.save()
+    print "Saved anaylsis for %s" % input_filename
 
-    # Get the first beats, just to test...
-    bars = audiofile.analysis.bars
-    collect = audio.AudioQuantumList()
-    for bar in bars:
-        collect.append(bar.children()[0])
-    out = audio.getpieces(audiofile, collect)
-    out.encode(output_filename)
 
 if __name__ == '__main__':
     import sys
     try:
         input_filename = sys.argv[1]
-        output_filename = sys.argv[2]
     except: 
         print usage
         sys.exit(-1)
-    main(input_filename, output_filename)
+    main(input_filename)
 
