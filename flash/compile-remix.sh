b7a61666b4eed6af53def8d22adf6c9da9c850aa
tkell <thor.kell@mail.mcgill.ca>
2013-01-18 14:20:30 -0500

    Removed flash.  Please use remix.js for web version of remix.

--- flash/compile-remix.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-../flex/flex_sdk/bin/mxmlc -source-path echo-nest-flash-api/src flash-audio/src MP3FileReferenceLoaderLib/src soundtouch-as3/src -- echo-nest-flash-api/examples/remix/remix.mxml 
-
--- flash/echo-nest-flash-api/examples/javascript/reverse.js
+++
@@ -1,9 +0,0 @@
-function remix (analysis) {
-  var beats = analysis.beats;
-  var samples = [];
-  for (var i = beats.length - 1; i > 0; i--) {
-    samples.push(beats[i - 1].value, beats[i].value);
-  }
-
-  return samples;
-}
