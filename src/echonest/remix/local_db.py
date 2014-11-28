a78353a5319f474f91031c6fe8d3f93a4ca66765
tkell <thor.kell@mail.mcgill.ca>
2014-11-28 09:27:37 -0500

    Cleaning up local_db

--- src/echonest/remix/local_db.py
+++ src/echonest/remix/local_db.py
@@ -6,13 +6,12 @@ local_db.py
 Functions for saving analysis and wave files to local storage
 """
 
-import sys
 import os
 import json
 import shutil
 import logging
 
-log = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 HOME = os.path.expanduser("~")
 REMIX_PATH = '.remix-db'
 REMIX_FOLDER = HOME + os.path.sep + REMIX_PATH
@@ -24,54 +23,52 @@ def check_and_create_local_db():
     '''If the local db does not exist, create it.'''
     home_dir = os.listdir(HOME)
     if REMIX_PATH in home_dir:
-        log.info("Found local database.")
+        LOG.info("Found local database.")
         return
     else:
-        log.info("Local database not found, creating...")
+        LOG.info("Local database not found, creating...")
 
         os.mkdir(REMIX_FOLDER)
         os.mkdir(AUDIO_FOLDER)
         os.mkdir(ANALYSIS_FOLDER)
 
-        f = open(DATABASE, 'wb')
-        f.close()
-        log.info("Local database created.")
+        db_file = open(DATABASE, 'wb')
+        db_file.close()
+        LOG.info("Local database created.")
 
 def check_db(track_md5):
-    # read the db, and see if the md5 is in it
-    with open(DATABASE, 'r') as f:
-        for line in f:
+    '''Check the DB and see if the track is in it.'''
+    with open(DATABASE, 'r') as db_file:
+        for line in db_file:
             if track_md5 == line.strip():
                 return True
     return False
 
 def save_to_local(track_md5, audio_file, pyechonest_track):
-    # write to the file
-    with open(DATABASE, 'a') as f:
-        f.write(track_md5 + '\n')
+    '''Save a track to the db.'''
+    with open(DATABASE, 'a') as db_file:
+        db_file.write(track_md5 + '\n')
 
     save_audio_to_local(track_md5, audio_file)
     save_analysis_to_local(track_md5, pyechonest_track)
 
 def save_audio_to_local(track_md5, audio_file):
-    # copy the audio file to the proper path, with the proper name
-    # This should be the straight-up file path
+    '''Copy the uncompressed audio file to the db.'''
     target_file = AUDIO_FOLDER + os.path.sep + track_md5 + '.wav'
     shutil.copyfile(audio_file, target_file)
 
 def save_analysis_to_local(track_md5, pyechonest_track):
-    # We get a pyechonest track object, and just dump the json of the dict
+    '''Save the pyechonest track dict as json to the db.'''
     target_file = ANALYSIS_FOLDER + os.path.sep + track_md5 + '.analysis'
-    with open(target_file, 'wb') as f:
-        json.dump(pyechonest_track.__dict__, f)
+    with open(target_file, 'wb') as db_file:
+        json.dump(pyechonest_track.__dict__, db_file)
 
 def get_audio_file(track_md5):
+    '''Get an audio file from the db.'''
     target_file = AUDIO_FOLDER + os.path.sep + track_md5 + '.wav'
     return target_file
 
 def get_analysis_file(track_md5):
+    '''Get an analysis file from the db.'''
     target_file = ANALYSIS_FOLDER + os.path.sep + track_md5 + '.analysis'
     return target_file
-        
-        
-
